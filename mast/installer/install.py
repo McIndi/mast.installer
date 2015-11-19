e5e0c6ed58f9d64fc6db980da21080eb1aea1323
ilovetux <me@ilovetux.com>
2015-11-19 16:07:52 -0500

    BUG: Remove pycrypto from net-install as it is included in Anaconda

--- mast/installer/install.py
+++ mast/installer/install.py
@@ -162,7 +162,6 @@ def _install_packages(prefix, net_install):
             "git+https://github.com/mcindi/mast.cli",
             "git+https://github.com/tellapart/commandr",
             "git+https://github.com/cherrypy/cherrypy",
-            "git+https://github.com/dlitz/pycrypto",
             "git+https://github.com/paramiko/paramiko",
             "git+https://github.com/waylan/Python-Markdown",
             "git+https://github.com/warner/python-ecdsa"
