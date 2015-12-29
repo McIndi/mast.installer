0b6bfa17523ce81b33881b59b6a3fee0bd4d6b0f
ilovetux <me@ilovetux.com>
2015-12-29 11:36:18 -0500

    merge xlsx

---
+++ mast/installer/files/contrib/merge-xlsx.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from mast.logging import make_logger
+from mast.cli import Cli
+import pandas as pd
+
+
+def main(left_file="",
+         right_file="",
+         out_file="",
+         on=[],
+         how="left"):
+    """given two xlsx (excel) workbooks, each containing one worksheet,
+join the two worksheets and output a new workbook.
+
+Parameters:
+
+* `-l, --left-file`: The workbook which contains the worksheet
+to consider the "left" table
+* `-r, --right-file`: The workbook which contains the worksheet
+to consider the "right" table
+* `-o, --out-file`: The file to output the "joined" tables to
+* `-O, --on`: A (space-seperated) list of column names to join on
+* `-H, --how`: how to join the two tables, must be one of "left",
+"right", "outer" or "inner"
+
+For more information on joining tables please see the
+[pandas dataframe merge documentation](http://pandas.pydata.org/pandas-docs/version/0.17.1/generated/pandas.DataFrame.merge.html)
+    """
+    left = pd.read_excel(left_file)
+    right = pd.read_excel(right_file)
+    new = pd.merge(left, right, on=on, how=how)
+    print "SAVING {}".format(out_file)
+    new.to_excel(out_file, index=False)
+
+
+if __name__ == "__main__":
+    cli = Cli(main=main, description=main.__doc__)
+    try:
+        cli.run()
+    except SystemExit:
+        pass
+    except:
+        make_logger("error").exception("An unhandled exception has occurred")
+        raise
