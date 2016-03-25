039c17c91a4a918e74340be91b189af74ebebc5b
Clifford Bressette <me@ilovetux.com>
2016-03-25 16:57:49 -0400

    Fix modify-config.py
    Many fixes here: Add support for nested attributes, make "vector-adds" more specific, add documentation, code cleanup and some efficiency improvements.

--- mast/installer/files/contrib/modify-config.py
+++ mast/installer/files/contrib/modify-config.py
@@ -1,11 +1,69 @@
 """
 modify-config.py
 
-POC for dynamically updating one or more attributes on groups of objects.
+POC for dynamically updating one or more attributes on groups of
+objects.
+
+To start you must specify the following options:
+
+* `-a, --appliances`: The IP/hostname/environment/alias of the
+appliances you wish to affect. Multiple entries must be in the form
+of `-a host-1 -a host2`
+* `-c, --credentials`: The credentials to use to authenticate to
+the DataPower(s). Either one for all or one each (must be in the same
+order as the appliances listed). Multiple entries must be in the form
+of `-c user:pass -c user:pass`
+* `-t, --timeout`: The timeout (in seconds) to wait for a response from
+an appliance.
+* `-n, --no-check-hostname`: If specified, ssl/tls verification will
+be disabled
+* `-d, --domains`: A list of domains to affect. You can pass in
+"all-domains" to affect all domains, or you can specify one or more
+domains by passing multiple arguments in the form of
+`-d domain-1 -d domain-2` 
+* `-o, --obj-class`: The class of objects to affect. Only one object
+class is permitted.
+* `-O, --obj-name`: The name of the object to affect, if not provided,
+affects all objects of class `obj-class`
+* `-D, --dry-run`: If specified, no requests will be sent to the
+appliances, instead the request that would've been sent to the
+appliances is printed to stdout
+* `-s, --save-config`: If specified, the configuration will be
+persisted for each domain affected.
+
+__Now for the interesting part__
+
+* `-m, --mods`: These are the modifications to make to each object.
+They should be in the form of "key=value" (I found it's best to keep
+the quotes and on linux sometimes you will need to use single-quotes
+to avoid shell expansion).
+
+If the attribute you wish to change is nested within the configuration
+you will need to use a "slash" seperator to seperate
+elements (similar to an xpath expression). For instance, if you wish
+to affect the "Disable-SSLv2" option of a CryptoProfile, you will need
+to have something like: `--mods "SSLOptions/Disable-SSLv2=on"`.
+
+If you need to perform a "vector-add", you will need to include that
+in the `--mods` argument. For instance, if you are adding a secondary
+address to an EthernetInterface, you would have pass in something like:
+`--mods "(vector-add)SecondaryAddress=1.2.3.4/24"`
+
+If the vector-add is nested, you will need to add the "(vector-add)"
+directive to the actual element being modified like this:
+`--mods "parent/(vector-add)child=value"`   
+
 """
+from collections import defaultdict
+from itertools import groupby
 from mast.datapower import datapower
 from mast.logging import make_logger
 from mast.cli import Cli
+from mast.pprint import pprint_xml_str
+
+
+def tree():
+    return defaultdict(tree)
 
 
 def main(appliances=[],
@@ -16,7 +74,6 @@ def main(appliances=[],
          obj_class="",
          obj_name="",
          mods=[],
-         vector_add=False,
          dry_run=False,
          save_config=False):
     check_hostname = not no_check_hostname
@@ -25,36 +82,57 @@ def main(appliances=[],
                                 credentials,
                                 timeout=timeout,
                                 check_hostname=check_hostname)
-    for appliance in env.appliances:
+    for appliance in env.appliances: 
         print appliance.hostname
         if "all-domains" in domains:
             domains = appliance.domains        
         for domain in domains:
             print "\t", domain
-            print obj_class, obj_name
+            print "\t\t", obj_class
             config = appliance.get_config(_class=obj_class,
                                           name=obj_name,
                                           domain=domain)
             objs = config.xml.findall(datapower.CONFIG_XPATH)
             for obj in objs:
                 name = obj.get("name")
-                print "\t\t{}".format(name)
+                print "\t\t\t{}".format(name)
                 appliance.request.clear()
                 request = appliance.request.request(domain=domain).modify_config()[obj_class](name=name)
+
+                parsed_mods = tree()
                 for mod in mods:
-                    key, value = mod.split("=")
-                    if vector_add:
-                        for node in obj.findall(key):
-                            request.append(node)
-                    request[key](value)
+                    k, v = mod.split("=")
+                    if "/" in k:
+                        ks = k.split("/")
+                        level = parsed_mods
+                        for _k in ks:
+                            last_level = level                            
+                            level = level[_k]
+                        last_level[_k] = v
+                    else:
+                        parsed_mods[k] = v
+                
+                def append_mods(request, mods):
+                    for k, v in mods.items():
+                        if k.startswith("(vector-add)"):
+                            k = k.replace("(vector-add)", "")
+                            for node in obj.findall(k):
+                                request.append(node)
+                        if isinstance(v, defaultdict):
+                            append_mods(request[k], v)
+                        else:
+                            request[k](v)
+                append_mods(request, parsed_mods)
+
                 if dry_run:
-                    print appliance.request
+                    pprint_xml_str(str(appliance.request))
                     continue
-                resp = appliance.send_request()
-                if resp:
-                    print "\t\t\tOK"
                 else:
-                    print "\t\t\tERROR, see log for details"
+                    resp = appliance.send_request()
+                if resp:
+                    print "\t\t\t\tOK"
+                else:
+                    print "\t\t\t\tERROR, see log for details"
             if save_config and not dry_run:
                 appliance.SaveConfig(domain=domain)
 
@@ -68,4 +146,3 @@ if __name__ == "__main__":
     except:
         make_logger("error").exception("An unhandled exception occurred")
         raise
-
