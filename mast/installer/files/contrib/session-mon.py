99e2370951c395e687882a205a8ee45a5dc6ed8d
Clifford Bressette <me@ilovetux.com>
2016-04-04 11:25:36 -0400

    Update session-mon.py

--- mast/installer/files/contrib/session-mon.py
+++ mast/installer/files/contrib/session-mon.py
@@ -12,7 +12,9 @@ import mast.datapower.datapower as datapower
 from mast.logging import make_logger
 from itertools import groupby
 from mast.cli import Cli
+import os
 
+__version__ = "{}-0".format(os.environ["MAST_VERSION"])
 
 class UserSession(object):
     def __init__(self, node):
