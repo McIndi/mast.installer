18c6e0d31c3daa08ac52f73eed3807cfc4c1a4b8
iLovetux <me@ilovetux.com>
2018-10-11 10:04:10 -0400

    Add missing dependency of lxml

--- mast/installer/resources.py
+++ mast/installer/resources.py
@@ -1,3 +1,4 @@
+
 import platform
 
 conda_dependencies = {
@@ -6,19 +7,23 @@ conda_dependencies = {
                   "pywin32": "https://anaconda.org/anaconda/pywin32/223/download/win-64/pywin32-223-py27h0c8e037_1.tar.bz2",
                   "vc": "https://anaconda.org/anaconda/vc/9/download/win-64/vc-9-h7299396_1.tar.bz2",
                   "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6.1/download/win-64/pycrypto-2.6.1-py27h0c8e037_9.tar.bz2",
+                  "lxml": "https://anaconda.org/anaconda/lxml/4.2.5/download/win-64/lxml-4.2.5-py27h31b8cb8_0.tar.bz2",
             },
             "32bit": {
                   "pywin32": "https://anaconda.org/anaconda/pywin32/223/download/win-32/pywin32-223-py27h0c8e037_1.tar.bz2",
                   "vc": "https://anaconda.org/anaconda/vc/9/download/win-32/vc-9-h7299396_1.tar.bz2",
                   "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6.1/download/win-32/pycrypto-2.6.1-py27_2.tar.bz2",
+                  "lxml": "https://anaconda.org/anaconda/lxml/4.2.5/download/win-32/lxml-4.2.5-py27h31b8cb8_0.tar.bz2",
             },
       },
       "Linux": {
             "64bit": {
                   "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6.1/download/linux-64/pycrypto-2.6.1-py27h14c3975_9.tar.bz2",
+                  "lxml": "https://anaconda.org/anaconda/lxml/4.1.1/download/linux-64/lxml-4.2.5-py27hdd00cef_0.tar.bz2",
             },
             "32bit": {
                   "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6.1/download/linux-32/pycrypto-2.6.1-py27_0.tar.bz2",
+                  "lxml": "https://anaconda.org/anaconda/lxml/4.1.1/download/linux-32/lxml-4.2.5-py27he277b01_0.tar.bz2",
             },
 
       },
