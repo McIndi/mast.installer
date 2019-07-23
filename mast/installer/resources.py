114db1fa6bd8062858c2d950fb70e73a4441c1ad
Clifford Bressette <cliffordbressette@mcindi.com>
2019-07-23 15:07:22 -0400

    Downgrade pycrypto to try to work around conda packaging issue

--- mast/installer/resources.py
+++ mast/installer/resources.py
@@ -5,19 +5,19 @@ conda_dependencies = {
       "Windows": {
             "64bit": {
                   "vc": "https://anaconda.org/anaconda/vc/14.1/download/win-64/vc-14.1-h0510ff6_4.tar.bz2",
-                  "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6.1/download/win-64/pycrypto-2.6.1-py27h0c8e037_9.tar.bz2",
+                  "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6/download/win-64/pycrypto-2.6-py27_0.tar.bz2",
             },
             "32bit": {
                   "vc": "https://anaconda.org/anaconda/vc/14.1/download/win-32/vc-14.1-h0510ff6_4.tar.bz2",
-                  "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6.1/download/win-32/pycrypto-2.6.1-py27_2.tar.bz2",
+                  "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6/download/win-32/pycrypto-2.6-py27_0.tar.bz2",
             },
       },
       "Linux": {
             "64bit": {
-                  "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6.1/download/linux-64/pycrypto-2.6.1-py27h14c3975_9.tar.bz2",
+                  "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6/download/linux-64/pycrypto-2.6-py27_0.tar.bz2",
             },
             "32bit": {
-                  "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6.1/download/linux-32/pycrypto-2.6.1-py27_0.tar.bz2",
+                  "pycrypto": "https://anaconda.org/anaconda/pycrypto/2.6/download/linux-32/pycrypto-2.6-py27_0.tar.bz2",
             },
 
       },
